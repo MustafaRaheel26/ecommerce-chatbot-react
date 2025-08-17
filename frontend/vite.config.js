0823074a3074b2ee36c1b9a1e9187e4935a40272
MustafaRaheel26 <mustafaraheel26@gmail.com>
2025-08-18 01:25:02 +0500

    new

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,11 +4,10 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	server: {
-		proxy: {
-			"/api": {
-				target: "http://localhost:5000",
-			},
-		},
-	},
+	build: {
+    outDir: "dist"
+  },
+  define: {
+    "process.env": {}   // prevents Rollup crashing on missing env
+  }
 });
